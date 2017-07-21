e42bbe46f7f507f59b4b0184b4a529e397ba3666
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-07-21 19:36:14 -0400

    Update index.nim

--- index.nim
+++ index.nim
@@ -1,3 +1,8 @@
 <!--{
 	print() "If you're seeing this, that means RK didn't *fully* give up :D, otherwise";
-}--> RK gave up >:(
+}--> RK gave up >:(<br/><br/>
+
+The UNIX epoch time is 
+<!--{
+	print() {epoch()}
+}-->

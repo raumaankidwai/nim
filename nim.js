6e27ced4c5d511be38ad6b887b13d461a64cf6d8
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-07-21 15:21:23 -0400

    Update nim.js

--- nim.js
+++ nim.js
@@ -1,9 +1,22 @@
+const fs = require("fs");
+
+function setConstProperty (obj, name, value) {
+	Object.defineProperty(obj, name, {
+		value: value,
+		writable: false,
+		enumerable: true,
+		configurable: true
+	});
+}
+
 function Server () {
-	this.index = "index.nim";
+	setConstProperty(this, "process", function (req, res) {
+		return fs.readFileSync(this.processURI(req.url)).toString();
+	});
 	
-	this.process = function (req, res) {
-		console.log(req.headers);
-	};
+	setConstProperty(this, "processURI", function (uri) {
+		return url;
+	});
 }
 
 module.exports = {

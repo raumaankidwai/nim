2aabf42d7e73c818554148f95d234aaf9b7fe0f6
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-07-21 15:36:24 -0400

    Update nim.js

--- nim.js
+++ nim.js
@@ -10,28 +10,33 @@ function setConstProperty (obj, name, value) {
 }
 
 function Server () {
-	var self = this;
-	
 	this.process = function (req, res) {
 		res.setHeader("Content-Type", "text/html");
 		
-		res.write(fs.readFileSync(self.processURI(req.url)).toString());
+		res.write(this.parser.process(fs.readFileSync(this.processURI(req.url)).toString()));
 		
 		res.end();
-	};
+	}.bind(this);
 	
 	this.processURI = function (uri) {
 		uri = uri.slice(1)
 		
 		if (!uri.length) {
-			return "index.nim";
-			//return self.index;
+			return this.index;
 		}
 		
 		return uri;
-	};
+	}.bind(this);
 	
 	this.index = "index.nim";
+	
+	this.parser = new Parser();
+}
+
+function Parser () {
+	this.process = function (text) {
+		
+	};
 }
 
 module.exports = {

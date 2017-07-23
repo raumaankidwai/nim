aef3a36bca98b2476afb800c55db31e626724bdc
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-07-23 14:33:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const Nim = require("./nim.js");
 
 const server = new Nim.NimServer();
 
-server.init();
+server.init({
+	absolute: "/home/pi/nim/site/",
+	index: "/index.nim"
+});
 
 http.createServer(server.process).listen(80);

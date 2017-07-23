8cfe35f9cc7c7e029d4f2790e2b3a93d9d5fac0e
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-07-23 11:57:29 -0400

    Update index.nim

--- site/index.nim
+++ site/index.nim
@@ -6,8 +6,8 @@ The UNIX epoch time is
 <!--{
 	$time = { epoch(); };
 	print() $time;
-}-->
+}--><br/><br/>
 
-Again, the epoch time is <!--{ print() $time; }-->
+Again, the epoch time is <!--{ print() $time; }--><br/>
 
 Click <a href="operations.nim">here</a> to look at operations!<br/>

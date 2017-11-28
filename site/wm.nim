d68afae50322e864521693eebec5cab97b7c72ca
Raumaan Kidwai <raumaankidwai@gmail.com>
2017-11-27 19:59:24 -0500

    Update wm.nim

--- site/wm.nim
+++ site/wm.nim
@@ -1,6 +1,8 @@
 <!--{
 
-model EventHandler {
+cursor() $ARROW;
+
+model "EventHandler" {
 	cons "you should not be using the return value of this function" $events {
 		$$events = [];
 		
@@ -23,5 +25,25 @@ model EventHandler {
 };
 
 $windows = [];
+$focusedWindow = -1;
+
+extend "EventHandler" "Window" {
+	cons "you should not be using the return value of this function" $app $x $y $w $h {
+		$$events = [];
+
+		for $i 0 { $i < $$model->eventsList->length; } { $i += 1; } {
+			$$events[$$model->eventsList[i]] = [];
+		}
+
+		$$zindex = 0;
+		
+		$$x = { $x ?? 100; };
+		$$y = { $y ?? 100; };
+		$$w = { $w ?? 200; };
+		$$h = { $h ?? 200; };
+		
+		$$app = $app;
+	};
+};
 
 }-->
